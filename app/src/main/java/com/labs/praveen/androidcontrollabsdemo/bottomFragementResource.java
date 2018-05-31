7d56c68f1008497452d5b3ba3532f57d585bde1b
Praveen Kumar Pandey <praveenkpandey@gmail.com>
2018-05-31 11:04:51 +0530

    Gesture, Fragments Commit

---
+++ app/src/main/java/com/labs/praveen/androidcontrollabsdemo/bottomFragementResource.java
@@ -0,0 +1,24 @@
+package com.labs.praveen.androidcontrollabsdemo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class bottomFragementResource extends Fragment{
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.bottom_fragment_resource, container, false);
+
+        return view;
+
+    }
+}
